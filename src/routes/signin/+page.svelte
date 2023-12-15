3af4fb6ce457d2a0ab0ada87a7040bc77e818cd2
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2023-12-15 11:18:42 +0800

    add auth

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -10,10 +10,10 @@
 		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight">📝 cvAI</h1>
 	</div>
 	<main class="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-4">
-		<h1 class="mx-auto max-w-[620px] font-display text-4xl font-bold sm:text-6xl mb-5">
+		<h1 class="mx-auto font-display text-4xl font-bold sm:text-6xl mb-5">
 			Redesign your <span class="">CV</span> in seconds
 		</h1>
-		<div class="h-[250px] flex flex-col items-center space-y-6 max-w-[670px]">
+		<div class="flex flex-col items-center space-y-6">
 			<div class="max-w-lg leading-8 text-base">
 				Sign in below to create a free account and redesign your CV today. You will get
 				<b>5 generations</b> for free.
@@ -22,18 +22,9 @@
 				supabaseClient={data.supabase}
 				providers={['github', 'google']}
 				redirectTo={`${data.url}/auth/callback`}
-				magicLink={true}
 				onlyThirdPartyProviders={true}
 				appearance={{
-					theme: ThemeSupa,
-					variables: {
-						default: {
-							colors: {
-								brand: '#0f172a',
-								brandAccent: '#fafafa'
-							}
-						}
-					}
+					theme: ThemeSupa
 				}}
 				theme="light"
 			/>

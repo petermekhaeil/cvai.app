394f17d25be1738dbadef98ed607599ecd4e2f06
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2023-12-16 09:10:22 +0800

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,8 +29,14 @@
 	<meta name="description" content="AI-powered resume and cover letter builder." />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:creator" content="@petermekh" />
-	<meta name="twitter:site" content="@petermekh" /><meta name="twitter:title" content="cvAI" />
+	<meta name="twitter:site" content="@petermekh" />
+	<meta name="twitter:title" content="📝 cvAI" />
 	<meta name="twitter:description" content="AI-powered resume and cover letter builder." />
 	<meta name="twitter:image" content="https://www.cvai.app/og.png" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="📝 cvAI" />
+	<meta property="og:description" content="AI-powered resume and cover letter builder." />
+	<meta property="og:url" content="https://www.cvai.app/" />
+	<meta property="og:image" content="https://www.cvai.app/og.png" />
 	<link rel="icon" href="https://fav.farm/📝" />
 </svelte:head>

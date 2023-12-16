3edef9624927891360811a11a98564beab66297b
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2023-12-16 10:42:44 +0800

    use @supabase/ssr (#5)

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,11 +1,21 @@
-import { redirect } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit';
 
-export const GET = async ({ url, locals: { supabase } }) => {
-  const code = url.searchParams.get('code')
+export const GET = async (event) => {
+  const {
+    url,
+    locals: { supabase }
+  } = event;
+  const code = url.searchParams.get('code') as string;
+  const next = url.searchParams.get('next') ?? '/';
 
   if (code) {
-    await supabase.auth.exchangeCodeForSession(code)
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      throw redirect(303, `/${next.slice(1)}`);
+    }
+    console.error(error);
   }
 
-  throw redirect(303, '/')
-}
+  // return the user to an error page with instructions
+  throw redirect(303, '/auth/auth-code-error');
+};

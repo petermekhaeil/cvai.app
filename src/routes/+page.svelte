1ef605d927ca6d661abb8d97fdf85130c6fead2c
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2024-01-07 08:19:52 +0800

    add history page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -181,7 +181,8 @@
 	<div class="sticky top-0 bg-white z-30 px-4 flex items-center justify-between py-2 border-b">
 		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight">📝 cvAI</h1>
 		{#if $page.data.session}
-			<div class="flex items-center sm:space-x-4 justify-end">
+			<div class="flex items-center justify-end">
+				<Button variant="link" href="/history" class="hidden sm:block">History</Button>
 				<Button variant="link">{credits} credit{credits > 1 ? 's' : ''} left</Button>
 				<UserNav />
 			</div>
